b0236ace098338144eaff981125b2104c5df5795
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-25 21:12:13 +0200

    Create a pdf and add page to this

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ filepaths = glob.glob("invoices/*.xlsx")
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
 
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+
+
