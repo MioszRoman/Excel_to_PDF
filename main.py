52571eaacef549921dba9fc898ebd6c1cf2971b3
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-25 21:11:03 +0200

    Get filepaths and read them using pandas in for loop

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from fpdf import FPDF
 import glob
 from pathlib import Path
 
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
